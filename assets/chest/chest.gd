37ed044b1de68222e2dc5e9889fcaf3e08bfd3e7
Alex <afandino99@gmail.com>
2025-04-07 21:54:58 +0200

    feat (models) add mythirl, ches, arc models

--- assets/chest/chest.gd
+++ assets/chest/chest.gd
@@ -1,16 +1,15 @@
 extends Area3D
 
+@onready var animation_tree: AnimationTree = $Mesh/AnimationTree
+
 var opened = false
 
 func _on_body_entered(body:Node3D) -> void:
   if body.is_in_group("MainPlayer"):
-      if opened:
-        return
-      
       open_chest()
 
 func open_chest():
-    pass;
+    animation_tree.set("parameters/open_chest/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
 
 func _on_player_enter_area(player: ) -> void:
   pass
