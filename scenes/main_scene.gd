f25e3c1129c9ad05e7c7cd6080bbab8c009a9fbd
Koldo <bordegaraikoldo@gmail.com>
2025-05-14 00:05:10 +0200

    feat(assets): New Chest, UI icons, Menu Videos

--- scenes/main_scene.gd
+++ scenes/main_scene.gd
@@ -1,29 +1,49 @@
 extends Control
 
 var level: int = 1
+@onready var video_player_1 = $Room
+@onready var video_player_2 = $Transition
+@onready var video_player_3 = $Book
+@onready var start_button = $Start
+@onready var title_image = $Title
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file(str("res://scenes/LvL 1.tscn"))
-	
 
 func _on_settings_pressed() -> void:
-	$CenterContainer/MainButtons.visible = false;
-	$CenterContainer/SettingsMenu.visible = true;
+	$CenterContainer/MainButtons.visible = false
+	$CenterContainer/SettingsMenu.visible = true
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 
-
 func _on_back_pressed() -> void:
-	$CenterContainer/MainButtons.visible = true;
-	$CenterContainer/SettingsMenu.visible = false;
+	$CenterContainer/MainButtons.visible = true
+	$CenterContainer/SettingsMenu.visible = false
 
 func _on_full_screen_toggled(toggled_on: bool) -> void:
 	if toggled_on:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
-	else: 
+	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
-
 
 func _on_main_vol_slider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Master"), value)
+
+func _on_start_button_down() -> void:
+	start_button.disabled = true
+	title_image.visible = false
+	video_player_1.stop()
+	video_player_1.visible = false
+	video_player_2.visible = true
+	video_player_2.stream = load("res://videos/Transition.ogv")
+	video_player_2.play()
+	video_player_2.connect("finished", Callable(self, "_on_video_2_finished"))
+
+func _on_video_2_finished():
+	video_player_2.stop()
+	video_player_2.visible = false
+	video_player_3.visible = true
+	video_player_3.stream = load("res://videos/Book.ogv")
+	video_player_3.loop = true
+	video_player_3.play()
