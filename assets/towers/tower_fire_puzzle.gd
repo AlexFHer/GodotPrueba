bb6f80ff157443ff2071d057018515a30b34f12f
Koldo <bordegaraikoldo@gmail.com>
2025-05-18 18:42:11 +0200

    feat(assets): castle door and lever

--- assets/towers/tower_fire_puzzle.gd
+++ assets/towers/tower_fire_puzzle.gd
@@ -1,13 +1,17 @@
 extends Node3D
 
+signal fire_activated 
+
 @onready var fire_tower: Node3D = $Fire_Tower_Model
+var activated := false
 
 func _ready():
-    pass
     fire_tower.visible = false
 
 func _on_area_entered(area: Area3D) -> void:
-    print("HIT");
-    if area.is_in_group("FireBall"):
+    if area.is_in_group("FireBall") and not activated:
+        print("HIT")
         fire_tower.visible = true
+        activated = true
+        fire_activated.emit()
         
