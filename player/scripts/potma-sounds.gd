cdf080fda75457487f3e0c93ceb9b471af359d74
Koldo <bordegaraikoldo@gmail.com>
2025-05-20 22:01:12 +0200

    feat(themes): main menu and game

--- player/scripts/potma-sounds.gd
+++ player/scripts/potma-sounds.gd
@@ -1,3 +1,9 @@
-class_name PotmaSounds extends Node
+class_name PotmaSounds extends Node;
 
 @export var walkSound: AudioStream;
+@export var runSound: AudioStream;
+@export var jumpSound: AudioStream;
+@export var superJump: AudioStream;
+@export var staffAttack: AudioStream;
+@export var fireBall: AudioStream;
+@export var drinkSound: AudioStream;
