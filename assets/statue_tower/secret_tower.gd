293b7a1aad1deaecaf4b693607d35b60d70ea2a4
Alex <afandino99@gmail.com>
2025-05-22 20:02:49 +0200

    feat(secre_tower): make secret tower move

--- assets/statue_tower/secret_tower.gd
+++ assets/statue_tower/secret_tower.gd
@@ -2,10 +2,19 @@ class_name SecretTower extends Node3D
 
 @onready var secret_tower: Node3D = $Model
 
+@export var activated_position_y := 0.0
+@export var move_speed := 0.2
+
 var activated := false
 
 func activate_secret_tower() -> void:
-  secret_tower.visible = true
-  activated = true
-  print("Secret tower activated")
+	secret_tower.visible = true
+	activated = true
+	print("Secret tower activated")
 
+func _process(delta: float) -> void:
+	if activated:
+		position.y = lerp(position.y, activated_position_y, move_speed * delta)
+		if abs(position.y - activated_position_y) < 0.1:
+			position.y = activated_position_y
+			activated = false
