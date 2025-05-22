b87a4a925e711fa1c0fe8ae569502105d0f45de8
Koldo <bordegaraikoldo@gmail.com>
2025-05-22 14:24:22 +0200

    assets(Sounds): Arc sound effect and Potma sound library

--- assets/arc/arc.gd
+++ assets/arc/arc.gd
@@ -1,41 +1,41 @@
-class_name Arc extends Area3D
+class_name Arc
+extends Area3D
 
 @onready var arcOn: Node3D = %ArcOn
 @onready var arcOff: Node3D = %ArcOff
-
-
+@export var arcActivated: AudioStreamPlayer3D
 @onready var deactiovationTimer: Timer = $DeactivationTimer
 
-var active = false;
-
-var maintainActive := false;
+var active = false
+var maintainActive := false
 
 signal active_changed(active: bool)
 
 func _ready() -> void:
-  deactiovationTimer.connect("timeout", _on_deactivation_timer_timeout)
-  deactivate()
+	deactiovationTimer.connect("timeout", _on_deactivation_timer_timeout)
+	deactivate()
 
 func activate() -> void:
-  active = true
-  arcOn.visible = true
-  arcOff.visible = false
-  active_changed.emit(true)
-  deactiovationTimer.start()
+	active = true
+	arcOn.visible = true
+	arcOff.visible = false
+	active_changed.emit(true)
+	deactiovationTimer.start()
+	arcActivated.play()
+	await arcActivated.finished
 
 func deactivate() -> void:
-  
-  if maintainActive:
-    return;
+	if maintainActive:
+		return
 
-  active = false
-  arcOn.visible = false
-  arcOff.visible = true
-  active_changed.emit(false)
+	active = false
+	arcOn.visible = false
+	arcOff.visible = true
+	active_changed.emit(false)
 
-func _on_body_entered(body:Node3D) -> void:
-  if body.is_in_group("MainPlayer"):
-    activate()
+func _on_body_entered(body: Node3D) -> void:
+	if body.is_in_group("MainPlayer"):
+		activate()
 
 func _on_deactivation_timer_timeout() -> void:
-  deactivate()
+	deactivate()
--- assets/npcs/npc_dialoge.gd
+++ assets/npcs/npc_dialoge.gd
@@ -10,24 +10,24 @@ var is_chatting: bool = false
 @export var dialogue_text: String = "Hola, soy un NPC."
 
 func _process(delta):
-    if player_in_chat_zone and Input.is_action_just_pressed("ui_accept") and not is_chatting:
-        start_dialogue()
+	if player_in_chat_zone and Input.is_action_just_pressed("ui_accept") and not is_chatting:
+		start_dialogue()
 
 func _on_body_entered(body: Node3D) -> void:
-    if body.name == "Player":
-        player = body
-        player_in_chat_zone = true
-        print("Jugador entró en zona de diálogo")
+	if body.name == "Player":
+		player = body
+		player_in_chat_zone = true
+		print("Jugador entró en zona de diálogo")
 
 func _on_body_exited(body: Node3D) -> void:
-    if body.name == "Player":
-        player_in_chat_zone = false
-        player = null
-        print("Jugador salió de zona de diálogo")
+	if body.name == "Player":
+		player_in_chat_zone = false
+		player = null
+		print("Jugador salió de zona de diálogo")
 
 func start_dialogue():
-    is_chatting = true
-    print(dialogue_text)
-    var dialogue_instance = dialogue_scene.instantiate()
-    get_tree().get_root().add_child(dialogue_instance)
-    dialogue_instance.d_file = dialogue_file 
+	is_chatting = true
+	print(dialogue_text)
+	var dialogue_instance = dialogue_scene.instantiate()
+	get_tree().get_root().add_child(dialogue_instance)
+	dialogue_instance.d_file = dialogue_file 
