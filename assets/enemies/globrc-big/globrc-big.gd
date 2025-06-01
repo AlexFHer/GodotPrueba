e9fdb24d793ae76598f160d3a32a60af5183e7c7
Alex <afandino99@gmail.com>
2025-06-01 18:16:50 +0200

    feat(globrc-big): add animations and hit

--- assets/enemies/globrc-big/globrc-big.gd
+++ assets/enemies/globrc-big/globrc-big.gd
@@ -4,16 +4,23 @@ const LEAVE_READY_STATE_TIME := 2.0
 const ROTATION_SENSIVITY := 4.0
 const DEFAULT_MITHRIL := 10
 
+const MELEE_ATTACK_COLLISION_DURATION := 0.2
+
 enum State {
 	Idle,
 	Ready,
-	Dead
+	Dead,
+	Attack
 }
 
 @onready var _rig: Node3D = %Rig
-@onready var leave_ready_timer: Timer = %LeaveReadyTimer
+@onready var _leave_ready_timer: Timer = %LeaveReadyTimer
+@onready var _animation_tree: AnimationTree = %AnimationTree
+
+@onready var attack_collider: CollisionShape3D = %MeleeAttackCollider
 
 @export var number_of_mythril: int = DEFAULT_MITHRIL
+@export var death_particles: PackedScene
 
 var state := State.Idle
 
@@ -22,14 +29,7 @@ var player: MainPlayer = null
 
 func _ready() -> void:
 	_set_state(State.Idle)
-
-func _set_state(new_state: State) -> void:
-	if state == State.Dead:
-		return
-	if state == new_state:
-		return
-		
-	state = new_state
+	disable_attack_collision()
 
 func _process(delta: float) -> void:
 	if state == State.Dead:
@@ -38,6 +38,31 @@ func _process(delta: float) -> void:
 	if state == State.Ready and isPlayerInRange:
 		_look_at_player(delta)
 
+func enable_attack_collision() -> void:
+	attack_collider.disabled = false
+
+func disable_attack_collision() -> void:
+	attack_collider.disabled = true
+
+func on_die_animation_finished() -> void:
+	_hide_mesh();
+	if death_particles:
+		var particles = death_particles.instantiate()
+		particles.global_position = _rig.global_position
+		get_tree().current_scene.add_child(particles)
+		particles.play()
+		# On particles end, we can remove the enemy
+		if particles.has_signal("finished"):
+			particles.connect("finished", _on_death_particles_finished)
+
+
+func _set_state(new_state: State) -> void:
+	if state == State.Dead:
+		return
+	if state == new_state:
+		return
+		
+	state = new_state
 func _look_at_player(delta: float) -> void:
 	if player == null or isPlayerInRange == false:
 		return
@@ -51,11 +76,7 @@ func _on_player_in_range_area_3d_body_entered(body:Node3D) -> void:
 		isPlayerInRange = true
 		player = body
 		_set_state(State.Ready)
-		leave_ready_timer.stop()
-
-func _on_player_in_range_area_3d_body_exited(body:Node3D) -> void:
-	if body is MainPlayer:
-		leave_ready_timer.start(LEAVE_READY_STATE_TIME)
+		_leave_ready_timer.stop()
 
 func _leave_ready_state() -> void:
 	isPlayerInRange = false
@@ -68,3 +89,30 @@ func _on_leave_ready_timer_timeout() -> void:
 func die() -> void:
 	CollectablesEmitterService.emitMithrilPickedUp(number_of_mythril)
 	_set_state(State.Dead)
+
+func _on_player_in_range_area_3d_body_exited(body:Node3D) -> void:
+	if body is MainPlayer:
+		_leave_ready_timer.start(LEAVE_READY_STATE_TIME)
+
+func _on_attack_range_area_3d_body_entered(body:Node3D) -> void:
+	if body is MainPlayer:
+		_set_state(State.Attack)
+
+func _on_attack_animation_finished() -> void:
+	pass
+
+func _on_attack_collision_area_3d_body_entered(body:Node3D) -> void:
+	if body is MainPlayer:
+		body.dealDamage()
+		disable_attack_collision()
+
+func _start_die_one_shot() -> void:
+	_animation_tree.set("parameters/DieOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+
+func _on_death_particles_finished() -> void:
+	queue_free()
+
+func _hide_mesh() -> void:
+	_rig.visible = false
+	_rig.collision_layer = 0
+	_rig.collision_mask = 0
